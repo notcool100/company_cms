e15e1fdeae726fe48c04d09bbc48866e23785c02
notcool100 <anjaljoshi6@gmail.com>
2025-04-25 22:03:07 +0545

    fix the error

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,7 @@
 import { NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import { prisma } from "@/lib/prisma"
-import { compare } from "bcrypt"
+import { compare } from "bcryptjs"
 
 export const authOptions: NextAuthOptions = {
   session: {
